ecdbe10fd658b65bbea8efd2e1eaf7470be87c80
Nicolás <n.a.casabona@gmail.com>
2020-04-16 23:49:25 -0300

    Donde vamos no necesitamos caminos

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='2048',
+    version='0.01',
+    install_requires=requirements,
+    url='https://github.com/nacasabona/2048',
+    python_requires='>=3.7',
+    license='MIT',
+    author='Nicolás Casabona and Juan Schandin',
+    author_email='n.a.casabona@gmail.com, juan.schandin@filo.uba.ar',
+    description='2048 Clone'
+)

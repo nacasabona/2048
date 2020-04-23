9f887d1f5cdcb8a5eb0ce366f3f87722dfc024f0
Nicolás <n.a.casabona@gmail.com>
2020-04-23 12:17:46 -0300

    Ahí cambié game

--- src/game.py
+++ src/game.py
@@ -11,16 +11,43 @@ logger = logging.getLogger(Path(__file__).stem)
 
 class Game:
     def __init__(self):
+        pg.init()
+        # después van a venir los ruiditos
+        pg.mixer.init()
         self.screen = pg.display.set_mode((s.WIDTH, s.HEIGHT))
         self.clock = pg.time.Clock()
 
+    def new(self):
+        # esto estaba en el modelo. después veo cómo meterlo cuando funcione el resto bien
+        # self.all_sprites = pg.sprite.Group()
+        pass
+
     def run(self):
         logger.info('Starting main loop')
-        running = True
-        while running:
+        self.running = True
+        while self.running:
             self.clock.tick(s.FPS)
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    running = False
-            self.screen.fill((0, 0, 0))
-            pg.display.flip()
+            self.events()
+            self.update()
+            self.draw()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+
+    def update(self):
+        # para cuando metamos las sprites
+        # self.all_sprites.update()
+        pass
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        # self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+
+
+
+
+

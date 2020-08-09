80364765bc5136e19fc478060a94bdfe6e4ea3e0
Juan Schandin <jschandin@gmail.com>
2020-08-09 19:17:49 -0300

    Use observer pattern

--- src/game.py
+++ src/game.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import pygame as pg
 
 import src.settings as s
+from src.events.overseer import Overseer
+from src.events.events import CustomEvents
 from src.utils.coords import Coords
 from src.utils.typewriter import TypewriterConfig, write
 
@@ -13,10 +15,15 @@ logger = logging.getLogger(Path(__file__).stem)
 
 class Game:
     def __init__(self):
+        self._register_listeners()
         self.screen = pg.display.set_mode((s.WIDTH, s.HEIGHT))
         self.clock = pg.time.Clock()
         self.running = False
         self.debug = False
+
+    def _register_listeners(self):
+        Overseer.add_listener(pg.QUIT, self)
+        Overseer.add_listener(pg.KEYDOWN, self)
 
     def new(self):
         # esto estaba en el modelo. después veo cómo meterlo cuando funcione el resto bien
@@ -28,20 +35,12 @@ class Game:
         self.running = True
         while self.running:
             self.clock.tick(s.FPS)
-            self.events()
             self.update()
             self.draw()
 
-    def events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_v:
-                    self.debug = not self.debug
-                    logger.info('Debug mode enabled: %s', self.debug)
-
     def update(self):
+        for event in pg.event.get():
+            Overseer.broadcast(event)
         self.screen.fill((0, 0, 0))
         if self.debug:
             self.draw_grid(self.screen)
@@ -50,6 +49,14 @@ class Game:
     def draw(self):
         # self.all_sprites.draw(self.screen)
         pg.display.flip()
+
+    def on_notify(self, event):
+        if event.type == pg.QUIT:
+            self.running = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_v:
+                self.debug = not self.debug
+                logger.info('Debug mode enabled: %s', self.debug)
 
     def draw_mouse_pos(self, screen):
         mx, my = pg.mouse.get_pos()

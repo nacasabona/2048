5e0db843c41b511601f62f0ce5f59eae0f1dc3ff
Juan Schandin <jschandin@gmail.com>
2020-08-14 23:56:16 -0300

    Add some convenient math functions for animations: clamp and linear interpolation

---
+++ src/utils/math.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+import pygame as pg
+
+
+def clamp(
+    min_: Union[float, int],
+    max_: Union[float, int],
+    val: Union[float, int]
+) -> Union[float, int]:
+    if val < min_:
+        return min_
+    if val > max_:
+        return max_
+    return val
+
+
+def lerp(
+    a: Union[float, int, pg.Vector2, pg.Vector3],
+    b: Union[float, int, pg.Vector2, pg.Vector3],
+    t: float
+) -> float:
+    return a * (1 - t) + b * t

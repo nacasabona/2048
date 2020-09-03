28fd71aa4f30c971d135f939e000b129174d4736
Juan Schandin <jschandin@gmail.com>
2020-09-03 19:31:55 -0300

    Remove events from boids feature

--- src/events/events.py
+++ src/events/events.py
@@ -3,11 +3,3 @@ import pygame as pg
 
 class CustomEvents:
     START_GAME = pg.event.custom_type()
-    PLAY_ANIMATION = pg.event.custom_type()
-    PAUSE_ANIMATION = pg.event.custom_type()
-    RESTART_ANIMATION = pg.event.custom_type()
-    W_ALIGN_CHANGE_ANIMATION = pg.event.custom_type()
-    W_COHESION_CHANGE_ANIMATION = pg.event.custom_type()
-    W_SEP_CHANGE_ANIMATION = pg.event.custom_type()
-    VIEW_RADIUS_CHANGE_ANIMATION = pg.event.custom_type()
-    MAX_STEER_FORCE_ANIMATION = pg.event.custom_type()
